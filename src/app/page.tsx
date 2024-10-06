665dd5be28fb3e39cc6acf7ace0fa44c56def77c
zzhuravleff <aqua197604@gmail.com>
2024-10-06 23:06:09 +0300

    переработка под Cron Job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import { createClient } from '@supabase/supabase-js';
-import cron from 'node-cron';
 
-const supabaseUrl = 'https://lduzndpbosjjueruqqee.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkdXpuZHBib3NqanVlcnVxcWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExNTQ5ODYsImV4cCI6MjAzNjczMDk4Nn0.rvC9GV8-HlWYQNgpU_OjPimcqpV0YLtwWPacAy7UvXA';
-const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-const fetchAndWriteData = async () => {
+export default async function handler(res: any) {
   try {
-    console.log('Fetching data from API...');
+    // Fetch data from API
     const response = await axios.get('https://api.mcstatus.io/v2/status/java/play.nexttime-mc.online');
-    console.log('Data received from API:');
     const data = response.data;
     const online = data.players.online;
 
-    // Записываем данные в таблицу Supabase
+    // Write data to Supabase
+    const supabaseUrl = 'https://lduzndpbosjjueruqqee.supabase.co';
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkdXpuZHBib3NqanVlcnVxcWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExNTQ5ODYsImV4cCI6MjAzNjczMDk4Nn0.rvC9GV8-HlWYQNgpU_OjPimcqpV0YLtwWPacAy7UvXA';
+    const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
     const { data: supabaseData, error } = await supabaseClient
       .from('stats')
       .insert({ value: online, ...data })
@@ -24,46 +21,20 @@ const fetchAndWriteData = async () => {
     if (error) {
       console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-const deleteOldData = async () => {
-  try {
-    const { data: supabaseData, error } = await supabaseClient
+    // Delete old data (more than 1 day)
+    const oneDayAgo = new Date(Date.now() - 86400000); // 86400000 - milliseconds in a day
+    const { data: oldData, error: oldError } = await supabaseClient
       .from('stats')
       .delete()
-      .lt('created_at', new Date(Date.now() - 86400000)); // 86400000 - количество миллисекунд в сутках
+      .eq('created_at', { lt: oneDayAgo.toISOString() });
 
-    if (error) {
-      console.error(error);
-      console.log('1');
+    if (oldError) {
+      console.error(oldError);
     }
   } catch (error) {
     console.error(error);
-    console.log('2');
   }
-};
 
-const cronJobFetchData = cron.schedule('*/1 * * * *', fetchAndWriteData, {
-  scheduled: true,
-  timezone: 'Europe/Moscow'
-});
-
-const cronJobDeleteOldData = cron.schedule('*/1 * * * *', () => {
-  console.log('Delete old data cron job started');
-  deleteOldData();
-});
-
-  cronJobFetchData.start();
-  cronJobDeleteOldData.start();
-
-
-export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
-  );
+  res.status(200).json({ message: 'Cron Job completed successfully' });
 }
